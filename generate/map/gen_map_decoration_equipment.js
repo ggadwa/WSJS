890688c0fd7f2f2f7fd557fd51a9105cc46f634f
Brian Barnes <ggadwa@charter.net>
2016-11-17 17:30:11 -0500

    11_17_2016

--- generate/map/gen_map_decoration_equipment.js
+++ generate/map/gen_map_decoration_equipment.js
@@ -1,4 +1,4 @@
-/* global map, config, MeshPrimitivesClass, MESH_FLAG_DECORATION, genRandom, DEGREE_TO_RAD */
+/* global map, config, MeshPrimitivesClass, MESH_FLAG_DECORATION, genRandom, DEGREE_TO_RAD, MeshUtilityClass, ROOM_SIDE_LEFT, ROOM_SIDE_RIGHT, ROOM_SIDE_TOP, ROOM_SIDE_BOTTOM */
 
 "use strict";
 
@@ -10,66 +10,195 @@ class GenRoomDecorationEquipmentClass
 {
     constructor()
     {
+        this.PIPE_SIDE_COUNT=12;
+        this.PIPE_CURVE_SEGMENT_COUNT=5;
+        
         Object.seal(this);
     }
     
         //
         // single pipe
         //
-        
-    addPipe(room,bitmap,pnt,radius,yBound)
-    {
-        var n,k,t,v,rd,tx,tz,tx2,tz2,bx,bz,bx2,bz2;
-        var u1,u2;
-        var pipeAng=new wsPoint(0,0,0);
-        var nextPnt=new wsPoint(0,0,0);
-        
-            // get cylinder pieces
-        
-        var sideCount=12;
-        var segCount=5;
-        
-        var vertexList=MeshUtilityClass.createMapVertexList(segCount*(sideCount*6));
-        var indexes=new Uint16Array(segCount*(sideCount*6));
 
-        var iCount=sideCount*6;
+    addPipeStraightChunk(bitmap,pnt,len,radius,pipeAng)
+    {
+        let n,v,rd,tx,tz,tx2,tz2,bx,bz,bx2,bz2;
+        let u1,u2;
+        let ang,ang2,angAdd;
+        let mesh,vertexList,indexes,iCount,vIdx;
+        let nextPnt=new wsPoint(0,0,0);
+        let addPnt=new wsPoint(0,0,0);
         
-        var vIdx=0;
-        var iIdx=0;
+            // get turn pieces
         
-            // cylinder segments
+        vertexList=MeshUtilityClass.createMapVertexList(this.PIPE_SIDE_COUNT*6);
+        indexes=new Uint16Array(this.PIPE_SIDE_COUNT*6);
+
+        iCount=this.PIPE_SIDE_COUNT*6;
+        
+        vIdx=0;
+        
+            // the end points
             
-        var yAdd=Math.trunc(yBound.getSize()/segCount);
-            
-        var ySegBound=yBound.copy();
-        ySegBound.min=ySegBound.max-yAdd;
+        nextPnt.setFromPoint(pnt);
+
+        addPnt.setFromValues(0,len,0);
+        addPnt.rotate(pipeAng);
+        nextPnt.addPoint(addPnt);
         
-        for (k=0;k!==segCount;k++) {
+            // cyliner faces
+
+        ang=0.0;
+        angAdd=360.0/this.PIPE_SIDE_COUNT;
+
+        for (n=0;n!==this.PIPE_SIDE_COUNT;n++) {
+            ang2=ang+angAdd;
+
+                // the two Us
+
+            u1=(ang*this.PIPE_SIDE_COUNT)/360.0;
+            u2=(ang2*this.PIPE_SIDE_COUNT)/360.0;
+
+                // force last segment to wrap
+
+            if (n===(this.PIPE_SIDE_COUNT-1)) ang2=0.0;
+
+            rd=ang*DEGREE_TO_RAD;
+            tx=nextPnt.x+((radius*Math.sin(rd))+(radius*Math.cos(rd)));
+            tz=nextPnt.z+((radius*Math.cos(rd))-(radius*Math.sin(rd)));
+
+            bx=pnt.x+((radius*Math.sin(rd))+(radius*Math.cos(rd)));
+            bz=pnt.z+((radius*Math.cos(rd))-(radius*Math.sin(rd)));
+
+            rd=ang2*DEGREE_TO_RAD;
+            tx2=nextPnt.x+((radius*Math.sin(rd))+(radius*Math.cos(rd)));
+            tz2=nextPnt.z+((radius*Math.cos(rd))-(radius*Math.sin(rd)));
+
+            bx2=pnt.x+((radius*Math.sin(rd))+(radius*Math.cos(rd)));
+            bz2=pnt.z+((radius*Math.cos(rd))-(radius*Math.sin(rd)));
+
+                // the points
+
+            v=vertexList[vIdx++];
+            v.position.setFromValues(tx,nextPnt.y,tz);
+            v.position.rotateAroundPoint(nextPnt,pipeAng);
+            v.normal.setFromSubPoint(v.position,nextPnt);
+            v.normal.normalize();
+            v.uv.setFromValues(u1,0.0);
+
+            v=vertexList[vIdx++];
+            v.position.setFromValues(tx2,nextPnt.y,tz2);
+            v.position.rotateAroundPoint(nextPnt,pipeAng);
+            v.normal.setFromSubPoint(v.position,nextPnt);
+            v.normal.normalize();
+            v.uv.setFromValues(u2,0.0);
+
+            v=vertexList[vIdx++];
+            v.position.setFromValues(bx,pnt.y,bz);
+            v.position.rotateAroundPoint(pnt,pipeAng);
+            v.normal.setFromSubPoint(v.position,pnt);
+            v.normal.normalize();
+            v.uv.setFromValues(u1,1.0);
+
+            v=vertexList[vIdx++];
+            v.position.setFromValues(tx2,nextPnt.y,tz2);
+            v.position.rotateAroundPoint(nextPnt,pipeAng);
+            v.normal.setFromSubPoint(v.position,nextPnt);
+            v.normal.normalize();
+            v.uv.setFromValues(u2,0.0);
+
+            v=vertexList[vIdx++];
+            v.position.setFromValues(bx2,pnt.y,bz2);
+            v.position.rotateAroundPoint(pnt,pipeAng);
+            v.normal.setFromSubPoint(v.position,pnt);
+            v.normal.normalize();
+            v.uv.setFromValues(u2,1.0);
+
+            v=vertexList[vIdx++];
+            v.position.setFromValues(bx,pnt.y,bz);
+            v.position.rotateAroundPoint(pnt,pipeAng);
+            v.normal.setFromSubPoint(v.position,pnt);
+            v.normal.normalize();
+            v.uv.setFromValues(u1,1.0);
+
+            ang=ang2;
+        }
+
+        for (n=0;n!==iCount;n++) {
+            indexes[n]=n;
+        }
+        
+            // calcualte the tangents
+
+        MeshUtilityClass.buildVertexListTangents(vertexList,indexes);
+
+            // finally create the mesh
+            // all cylinders are simple box collisions
+
+        mesh=new MapMeshClass(bitmap,vertexList,indexes,MESH_FLAG_DECORATION);
+        mesh.simpleCollisionGeometry=true;
+        
+        map.addMesh(mesh);
+    }
+
+    addPipeCornerChunk(bitmap,pnt,radius,xTurn,zTurn)
+    {
+        let n,k,v,rd,tx,tz,tx2,tz2,bx,bz,bx2,bz2;
+        let yAdd,xTurnAdd,zTurnAdd;
+        let u1,u2;
+        let ang,ang2,angAdd;
+        let mesh,vertexList,indexes,iCount,vIdx,iIdx;
+        let pipeAng=new wsPoint(0,0,0);
+        let nextPipeAng=new wsPoint(0,0,0);
+        let nextPnt=new wsPoint(0,0,0);
+        let addPnt=new wsPoint(0,0,0);
+        
+            // get turn pieces
+        
+        vertexList=MeshUtilityClass.createMapVertexList(this.PIPE_CURVE_SEGMENT_COUNT*(this.PIPE_SIDE_COUNT*6));
+        indexes=new Uint16Array(this.PIPE_CURVE_SEGMENT_COUNT*(this.PIPE_SIDE_COUNT*6));
+
+        iCount=this.PIPE_SIDE_COUNT*6;
+        
+        vIdx=0;
+        iIdx=0;
+        
+            // turn segments
+            
+        yAdd=Math.trunc((radius*2)/this.PIPE_CURVE_SEGMENT_COUNT);
+        xTurnAdd=xTurn/this.PIPE_CURVE_SEGMENT_COUNT;
+        zTurnAdd=zTurn/this.PIPE_CURVE_SEGMENT_COUNT;
+        
+        angAdd=360.0/this.PIPE_SIDE_COUNT;
+        
+        for (k=0;k!==this.PIPE_CURVE_SEGMENT_COUNT;k++) {
             
             nextPnt.setFromPoint(pnt);
-            pnt.y-=1000;
-            nextPnt.rotateAroundPoint(pnt,pipeAng);
             
-            pipeAng.x=genRandom.randomInt(0,50);
-            pipeAng.z=genRandom.randomInt(0,50);
+            addPnt.setFromValues(0,-yAdd,0);
+            addPnt.rotate(pipeAng);
+            nextPnt.addPoint(addPnt);
+            
+            nextPipeAng.setFromPoint(pipeAng);
+            nextPipeAng.x+=xTurnAdd;
+            nextPipeAng.z+=zTurnAdd;
+            
 
                 // cyliner faces
 
-            var ang=0.0;
-            var ang2;
-            var angAdd=360.0/sideCount;
+            ang=0.0;
 
-            for (n=0;n!==sideCount;n++) {
+            for (n=0;n!==this.PIPE_SIDE_COUNT;n++) {
                 ang2=ang+angAdd;
                 
                     // the two Us
                     
-                u1=(ang*segCount)/360.0;
-                u2=(ang2*segCount)/360.0;
+                u1=(ang*this.PIPE_SIDE_COUNT)/360.0;
+                u2=(ang2*this.PIPE_SIDE_COUNT)/360.0;
 
                     // force last segment to wrap
                     
-                if (n===(sideCount-1)) ang2=0.0;
+                if (n===(this.PIPE_SIDE_COUNT-1)) ang2=0.0;
 
                 rd=ang*DEGREE_TO_RAD;
                 tx=nextPnt.x+((radius*Math.sin(rd))+(radius*Math.cos(rd)));
@@ -87,38 +216,47 @@ class GenRoomDecorationEquipmentClass
                 
                     // the points
                 
-                v=vertexList[vIdx];
-                v.position.setFromValues(tx,ySegBound.min,tz);
+                v=vertexList[vIdx++];
+                v.position.setFromValues(tx,nextPnt.y,tz);
+                v.position.rotateAroundPoint(nextPnt,nextPipeAng);
+                v.normal.setFromSubPoint(v.position,nextPnt);
+                v.normal.normalize();
                 v.uv.setFromValues(u1,0.0);
                 
-                v=vertexList[vIdx+1];
-                v.position.setFromValues(tx2,ySegBound.min,tz2);
+                v=vertexList[vIdx++];
+                v.position.setFromValues(tx2,nextPnt.y,tz2);
+                v.position.rotateAroundPoint(nextPnt,nextPipeAng);
+                v.normal.setFromSubPoint(v.position,nextPnt);
+                v.normal.normalize();
                 v.uv.setFromValues(u2,0.0);
                 
-                v=vertexList[vIdx+2];
-                v.position.setFromValues(bx,ySegBound.max,bz);
+                v=vertexList[vIdx++];
+                v.position.setFromValues(bx,pnt.y,bz);
+                v.position.rotateAroundPoint(pnt,pipeAng);
+                v.normal.setFromSubPoint(v.position,pnt);
+                v.normal.normalize();
                 v.uv.setFromValues(u1,1.0);
                 
-                v=vertexList[vIdx+3];
-                v.position.setFromValues(tx2,ySegBound.min,tz2);
+                v=vertexList[vIdx++];
+                v.position.setFromValues(tx2,nextPnt.y,tz2);
+                v.position.rotateAroundPoint(nextPnt,nextPipeAng);
+                v.normal.setFromSubPoint(v.position,nextPnt);
+                v.normal.normalize();
                 v.uv.setFromValues(u2,0.0);
                 
-                v=vertexList[vIdx+4];
-                v.position.setFromValues(bx2,ySegBound.max,bz2);
+                v=vertexList[vIdx++];
+                v.position.setFromValues(bx2,pnt.y,bz2);
+                v.position.rotateAroundPoint(pnt,pipeAng);
+                v.normal.setFromSubPoint(v.position,pnt);
+                v.normal.normalize();
                 v.uv.setFromValues(u2,1.0);
                 
-                v=vertexList[vIdx+5];
-                v.position.setFromValues(bx,ySegBound.max,bz);
+                v=vertexList[vIdx++];
+                v.position.setFromValues(bx,pnt.y,bz);
+                v.position.rotateAroundPoint(pnt,pipeAng);
+                v.normal.setFromSubPoint(v.position,pnt);
+                v.normal.normalize();
                 v.uv.setFromValues(u1,1.0);
-                
-                    // the normals
-                    
-                for (t=0;t!==6;t++) {
-                    v=vertexList[vIdx++];
-                    v.normal.setFromSubPoint(v.position,pnt);       // supergumba -- this needs to be rewritten
-                    v.normal.y=0.0;
-                    v.normal.normalize();
-                }
                 
                 ang=ang2;
             }
@@ -129,10 +267,8 @@ class GenRoomDecorationEquipmentClass
             
             iIdx+=iCount;
             
-            ySegBound.max=ySegBound.min;
-            ySegBound.min-=yAdd;
-            
             pnt.setFromPoint(nextPnt);
+            pipeAng.setFromPoint(nextPipeAng);
         }
         
             // calcualte the tangents
@@ -142,10 +278,69 @@ class GenRoomDecorationEquipmentClass
             // finally create the mesh
             // all cylinders are simple box collisions
 
-        var mesh=new MapMeshClass(bitmap,vertexList,indexes,MESH_FLAG_DECORATION);
-        //mesh.simpleCollisionGeometry=true;
+        mesh=new MapMeshClass(bitmap,vertexList,indexes,MESH_FLAG_DECORATION);
+        mesh.simpleCollisionGeometry=true;
         
-        return(mesh);
+        map.addMesh(mesh);
+    }
+        
+    addPipe(bitmap,dir,pnt,radius,dirLen,yBound)
+    {
+        let len,pipeAng;
+        
+            // pipes always start up
+            
+        pipeAng=new wsPoint(0,0,180.0);     // force len to point up
+        len=genRandom.randomInt(config.ROOM_FLOOR_DEPTH,(yBound.getSize()-(config.ROOM_FLOOR_DEPTH+(radius*2))));
+        this.addPipeStraightChunk(bitmap,pnt,len,radius,pipeAng);
+        
+        pnt.y-=len;
+        
+            // the turn and exit
+            
+        switch (dir) {
+            
+            case ROOM_SIDE_LEFT:  
+                this.addPipeCornerChunk(bitmap,pnt,radius,0.0,-90.0);
+
+                pipeAng.setFromValues(0.0,0.0,90.0);
+                this.addPipeStraightChunk(bitmap,pnt,dirLen,radius,pipeAng);
+                
+                pnt.x-=dirLen;
+                //this.addPipeCornerChunk(bitmap,pnt,radius,0.0,90.0);
+                break;
+                
+            case ROOM_SIDE_RIGHT:
+                this.addPipeCornerChunk(bitmap,pnt,radius,0.0,90.0);
+
+                pipeAng.setFromValues(0.0,0.0,-90.0);
+                this.addPipeStraightChunk(bitmap,pnt,dirLen,radius,pipeAng);
+                
+                pnt.x+=dirLen;
+                //this.addPipeCornerChunk(bitmap,pnt,radius,0.0,-90.0);
+                break;
+                
+            case ROOM_SIDE_TOP:
+                this.addPipeCornerChunk(bitmap,pnt,radius,90.0,0.0);
+
+                pipeAng.setFromValues(-90.0,0.0,0.0);
+                this.addPipeStraightChunk(bitmap,pnt,dirLen,radius,pipeAng);
+                
+                pnt.z-=dirLen;
+                //this.addPipeCornerChunk(bitmap,pnt,radius,-90.0,0.0);
+                break;
+                
+            case ROOM_SIDE_BOTTOM:
+                this.addPipeCornerChunk(bitmap,pnt,radius,-90.0,0.0);
+
+                pipeAng.setFromValues(90.0,0.0,0.0);
+                this.addPipeStraightChunk(bitmap,pnt,dirLen,radius,pipeAng);
+                
+                pnt.z+=dirLen;
+                //this.addPipeCornerChunk(bitmap,pnt,radius,90.0,0.0);
+                break;
+        }
+        
     }
         
         //
@@ -154,21 +349,24 @@ class GenRoomDecorationEquipmentClass
         
     addPipes(room)
     {
-        var n,pos,yBound,platformBoundX,platformBoundY,platformBoundZ,pipeBoundY;
-        var nPipe,pipeWid,radius,wid;
-        var ang,angAdd,rd;
-        var platformBitmap,pipeBitmap;
-        var centerPt;
+        let n,pos,yBound,platformBoundX,platformBoundY,platformBoundZ,pipeBoundY;
+        let nPipe,pipeWid,radius,wid;
+        let ang,angAdd,rd;
+        let platformBitmap,pipeBitmap;
+        let centerPt,dir,dirLen,distLft,distRgt,distTop,distBot;
         
             // the pipes location
 
         pos=room.findAndBlockSpawnPosition(true);
         if (pos===null) return;
         
+        pipeWid=Math.trunc(config.ROOM_BLOCK_WIDTH*0.25);
+        radius=Math.trunc(config.ROOM_BLOCK_WIDTH*0.1);
+        
         platformBitmap=map.getTexture(map.TEXTURE_TYPE_PLATFORM);
         pipeBitmap=map.getTexture(map.TEXTURE_TYPE_METAL);
         
-            // the pipe platforms
+            // the pipe platform
         
         yBound=room.getGruondFloorSpawnToFirstPlatformOrTopBoundByCoordinate(pos);
         
@@ -176,14 +374,42 @@ class GenRoomDecorationEquipmentClass
 
         platformBoundX=new wsBound((pos.x-wid),(pos.x+wid));
         platformBoundZ=new wsBound((pos.z-wid),(pos.z+wid));
-
-        //platformBoundY=new wsBound(yBound.min,(yBound.min+config.ROOM_FLOOR_DEPTH));
-        //map.addMesh(MeshPrimitivesClass.createMeshCube(platformBitmap,platformBoundX,platformBoundY,platformBoundZ,null,false,true,true,true,true,false,true,false,MESH_FLAG_DECORATION));
         
         platformBoundY=new wsBound((yBound.max-config.ROOM_FLOOR_DEPTH),room.yBound.max);
         map.addMesh(MeshPrimitivesClass.createMeshCube(platformBitmap,platformBoundX,platformBoundY,platformBoundZ,null,false,true,true,true,true,true,false,false,MESH_FLAG_DECORATION));
 
         pipeBoundY=new wsBound((yBound.min+config.ROOM_FLOOR_DEPTH),(yBound.max-config.ROOM_FLOOR_DEPTH));
+        
+            // determine direction
+            
+        distLft=pos.x-room.xBound.min;
+        distRgt=room.xBound.max-pos.x;
+        distTop=pos.z-room.zBound.min;
+        distBot=room.zBound.max-pos.z;
+        
+        if ((distLft<distRgt) && (distLft<distTop) && (distLft<distBot)) {
+            dir=ROOM_SIDE_LEFT;
+            dirLen=distLft;
+        }
+        else {
+            if ((distRgt<distTop) && (distRgt<distBot)) {
+                dir=ROOM_SIDE_RIGHT;
+                dirLen=distRgt;
+            }
+            else {
+                if (distTop<distBot) {
+                    dir=ROOM_SIDE_TOP;
+                    dirLen=distTop;
+                }
+                else {
+                    dir=ROOM_SIDE_BOTTOM;
+                    dirLen=distBot;
+                }
+            }
+        }
+        
+        dirLen-=Math.trunc((config.ROOM_BLOCK_WIDTH*0.5)+radius);
+        if (dirLen<0) dirLen=100;
         
             // create the pipes
             
@@ -194,16 +420,13 @@ class GenRoomDecorationEquipmentClass
         ang=0.0;
         angAdd=360.0/nPipe;
 
-        pipeWid=Math.trunc(config.ROOM_BLOCK_WIDTH*0.25);
-        radius=Math.trunc(config.ROOM_BLOCK_WIDTH*0.1);
-
         for (n=0;n!==nPipe;n++) {
             rd=ang*DEGREE_TO_RAD;
             centerPt.x=pos.x+((pipeWid*Math.sin(rd))+(pipeWid*Math.cos(rd)));
+            centerPt.y=pipeBoundY.max;
             centerPt.z=pos.z+((pipeWid*Math.cos(rd))-(pipeWid*Math.sin(rd)));
 
-            map.addMesh(this.addPipe(room,pipeBitmap,centerPt,radius,pipeBoundY));
-            //map.addMesh(MeshPrimitivesClass.createMeshCylinderSimple(pipeBitmap,centerPt,pipeBoundY,radius,MESH_FLAG_DECORATION));
+            this.addPipe(pipeBitmap,dir,centerPt,radius,dirLen,pipeBoundY);
 
             ang+=angAdd;
         }
@@ -215,7 +438,7 @@ class GenRoomDecorationEquipmentClass
 
     create(room)
     {
-        var n,pieceCount;
+        let n,pieceCount;
         
         pieceCount=room.getDecorationCount();
 
