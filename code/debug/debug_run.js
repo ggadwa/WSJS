ea9c7f7546fd9c1adbf290e4ca16efc911f1d206
Brian Barnes <ggadwa@charter.net>
2017-03-29 17:22:23 -0400

    03_29_2017

--- code/debug/debug_run.js
+++ code/debug/debug_run.js
@@ -1,10 +1,13 @@
+/* global sound */
+
 "use strict";
 
 class DebugItemClass
 {
-    constructor(name,objType,obj)
+    constructor(name,typeIdx,objType,obj)
     {
         this.name=name;
+        this.typeIdx=typeIdx;
         this.objType=objType;
         this.obj=obj;
     }
@@ -19,25 +22,10 @@ class DebugRunClass
     constructor()
     {
         this.listDiv=null;
-        this.canvas=null;
-        
-        this.drawTop=5;
-        this.bitmapWid=1200;
-        this.bitmapHigh=400;
-        
-        this.genBitmapWall=new GenBitmapWallClass();        // supergumba -- delete me later
-        this.genBitmapFloor=new GenBitmapFloorClass();
-        this.genBitmapCeiling=new GenBitmapCeilingClass();
-        this.genBitmapDoor=new GenBitmapDoorClass();
-        this.genBitmapMetal=new GenBitmapMetalClass();
-        this.genBitmapMachine=new GenBitmapMachineClass();
-        this.genBitmapPanel=new GenBitmapPanelClass();
-        this.genBitmapBox=new GenBitmapBoxClass();
-        this.genBitmapLiquid=new GenBitmapLiquidClass();
-        this.genBitmapSkin=new GenBitmapSkinClass();
-        this.genBitmapItem=new GenBitmapItemClass();
-        this.genBitmapSky=new GenBitmapSkyClass();
-        this.genBitmapParticle=new GenBitmapParticleClass();
+        this.lastItemDiv=null;
+        this.bitmapCanvas=null;
+        this.soundCanvas=null;
+        this.currentSoundBuffer=null;
         
         this.DEBUG_ITEM_TYPE_BITMAP=0;
         this.DEBUG_ITEM_TYPE_SOUND=1;
@@ -45,229 +33,137 @@ class DebugRunClass
         
         this.list=[];
 
-        this.list.push(new DebugItemClass('Wall',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapWallClass()));
-        this.list.push(new DebugItemClass('Floor',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapFloorClass()));
-        this.list.push(new DebugItemClass('Ceiling',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapCeilingClass()));
-        this.list.push(new DebugItemClass('Door',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapDoorClass()));
-        this.list.push(new DebugItemClass('Metal',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapMetalClass()));
-        this.list.push(new DebugItemClass('Machine',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapMachineClass()));
-        this.list.push(new DebugItemClass('Panel',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapPanelClass()));
-        this.list.push(new DebugItemClass('Box',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapBoxClass()));
-        this.list.push(new DebugItemClass('Liquid',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapLiquidClass()));
-        this.list.push(new DebugItemClass('Skin',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapSkinClass()));
-        this.list.push(new DebugItemClass('Item',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapItemClass()));
-        this.list.push(new DebugItemClass('Sky',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapSkyClass()));
-        this.list.push(new DebugItemClass('Particle',this.DEBUG_ITEM_TYPE_BITMAP,new GenBitmapParticleClass()));
+        this.fillListWithBitmapGenerator('Wall',new GenBitmapWallClass());
+        this.fillListWithBitmapGenerator('Floor',new GenBitmapFloorClass());
+        this.fillListWithBitmapGenerator('Ceiling',new GenBitmapCeilingClass());
+        this.fillListWithBitmapGenerator('Door',new GenBitmapDoorClass());
+        this.fillListWithBitmapGenerator('Metal',new GenBitmapMetalClass());
+        this.fillListWithBitmapGenerator('Machine',new GenBitmapMachineClass());
+        this.fillListWithBitmapGenerator('Panel',new GenBitmapPanelClass());
+        this.fillListWithBitmapGenerator('Box',new GenBitmapBoxClass());
+        this.fillListWithBitmapGenerator('Liquid',new GenBitmapLiquidClass());
+        this.fillListWithBitmapGenerator('Skin',new GenBitmapSkinClass());
+        this.fillListWithBitmapGenerator('Item',new GenBitmapItemClass());
+        this.fillListWithBitmapGenerator('Sky',new GenBitmapSkyClass());
+        this.fillListWithBitmapGenerator('Particle',new GenBitmapParticleClass());
+        
+        sound.initialize();
+        this.fillListWithSoundGenerator(new GenSoundClass(sound.getAudioContext()));
         
         Object.seal(this);
     }
     
         //
-        // draw single bitmap
+        // break up generator objects by their type names
         //
         
-    drawSingleBitmap(genName,typeName,debugBitmap)
+    fillListWithBitmapGenerator(name,obj)
     {
-        let canvas,ctx,div;
-        let wid=Math.trunc(this.bitmapWid/3);
-
-            // label
-
-        div=document.createElement('div');
-        div.style.position="absolute";
-        div.style.left='5px';
-        div.style.top=this.drawTop+'px';
-        div.innerHTML=genName+': '+typeName;
-        document.body.appendChild(div);
-
-        this.drawTop+=25;
-
-        canvas=document.createElement('canvas');
-        canvas.style.position="absolute";
-        canvas.style.left='5px';
-        canvas.style.top=this.drawTop+'px';
-        canvas.style.border='1px solid #000000';
-        canvas.width=this.bitmapWid;
-        canvas.height=this.bitmapHigh;
-
-        ctx=canvas.getContext('2d');
-        ctx.drawImage(debugBitmap.bitmap,0,0,wid,this.bitmapHigh);
-        if (debugBitmap.normal!==null) ctx.drawImage(debugBitmap.normal,wid,0,wid,this.bitmapHigh);
-        if (debugBitmap.specular!==null) ctx.drawImage(debugBitmap.specular,(wid*2),0,wid,this.bitmapHigh);
-
-        document.body.appendChild(canvas);
-
-        this.drawTop+=(this.bitmapHigh+5);
+        let n;
+        
+        for (n=0;n!==obj.TYPE_NAMES.length;n++) {
+            this.list.push(new DebugItemClass(name,n,this.DEBUG_ITEM_TYPE_BITMAP,obj));
+        }
     }
     
+    fillListWithSoundGenerator(obj)
+    {
+        let n;
+        
+        for (n=0;n!==obj.TYPE_NAMES.length;n++) {
+            this.list.push(new DebugItemClass(null,n,this.DEBUG_ITEM_TYPE_SOUND,obj));
+        }
+    }
+        
         //
-        // bitmap types
+        // item drawing
         //
         
-    addBitmapWall(idx)
+    drawBitmap(item)
     {
-        this.drawSingleBitmap('Wall',this.genBitmapWall.TYPE_NAMES[idx],this.genBitmapWall.generate(idx,true));
+        let ctx,debugBitmap;
+        let wid=this.bitmapCanvas.width;
+        let high=this.bitmapCanvas.height;
+        
+            // generate the bitmap
+            
+        debugBitmap=item.obj.generate(item.typeIdx,true);
+        
+            // draw the bitmap
+            
+        ctx=this.bitmapCanvas.getContext('2d');
+        ctx.fillStyle='#FFFFFF';
+        ctx.fillRect(0,0,wid,high);
+        
+        ctx.drawImage(debugBitmap.bitmap,0,0,Math.trunc(wid/2),Math.trunc(high/2));
+        if (debugBitmap.normal!==null) ctx.drawImage(debugBitmap.normal,Math.trunc(wid/2),0,Math.trunc(wid/2),Math.trunc(high/2));
+        if (debugBitmap.specular!==null) ctx.drawImage(debugBitmap.specular,0,Math.trunc(high/2),Math.trunc(wid/2),Math.trunc(high/2));
+        
+            // show the canvas
+            
+        this.soundCanvas.style.display='none';
+        this.bitmapCanvas.style.display='';
+    }
+    
+    drawSound(item)
+    {
+        let x,dataSkip,dataLen,y,idx,halfHigh;
+        let soundBuffer,data;
+        let ctx;
+        let wid=this.soundCanvas.width;
+        let high=this.soundCanvas.height;
+        
+        this.currentSoundBuffer=item.obj.generate(item.typeIdx,true);
+        data=this.currentSoundBuffer.buffer.getChannelData(0);
+        dataLen=data.length;
+        
+        ctx=this.soundCanvas.getContext('2d');
+        ctx.fillStyle='#FFFFFF';
+        ctx.fillRect(0,0,wid,high);
+        
+            // the midline
+            
+        halfHigh=Math.trunc(high/2);
+            
+        ctx.strokeStyle='#FF0000';
+        
+        ctx.beginPath();
+        ctx.moveTo(0,halfHigh);
+        ctx.lineTo(wid,halfHigh);
+        ctx.stroke();
 
-        idx++;
-        if (idx>=this.genBitmapWall.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapFloor.bind(this,0),1);
-            return;
-        }
-        
-        setTimeout(this.addBitmapWall.bind(this,idx),1);
-    }
-    
-    addBitmapFloor(idx)
-    {
-        this.drawSingleBitmap('Floor',this.genBitmapFloor.TYPE_NAMES[idx],this.genBitmapFloor.generate(idx,true));
+            // the drawing skip
 
-        idx++;
-        if (idx>=this.genBitmapFloor.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapCeiling.bind(this,0),1);
-            return;
-        }
-        
-        setTimeout(this.addBitmapFloor.bind(this,idx),1);
-    }
-    
-    addBitmapCeiling(idx)
-    {
-        this.drawSingleBitmap('Ceiling',this.genBitmapCeiling.TYPE_NAMES[idx],this.genBitmapCeiling.generate(idx,true));
+        dataSkip=Math.trunc(dataLen/wid);
 
-        idx++;
-        if (idx>=this.genBitmapCeiling.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapDoor.bind(this,0),1);
-            return;
-        }
-        
-        setTimeout(this.addBitmapCeiling.bind(this,idx),1);
-    }
-    
-    addBitmapDoor(idx)
-    {
-        this.drawSingleBitmap('Door',this.genBitmapDoor.TYPE_NAMES[idx],this.genBitmapDoor.generate(idx,true));
+            // draw the wave
 
-        idx++;
-        if (idx>=this.genBitmapDoor.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapMetal.bind(this,0),1);
-            return;
-        }
-        
-        setTimeout(this.addBitmapDoor.bind(this,idx),1);
-    }
-    
-    addBitmapMetal(idx)
-    {
-        this.drawSingleBitmap('Metal',this.genBitmapMetal.TYPE_NAMES[idx],this.genBitmapMetal.generate(idx,true));
+        ctx.strokeStyle='#0000FF';
+        ctx.beginPath();
 
-        idx++;
-        if (idx>=this.genBitmapMetal.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapMachine.bind(this,0),1);
-            return;
+        y=halfHigh+Math.trunc(data[0]*halfHigh);
+        ctx.moveTo(0,y);
+        
+        idx=0;
+
+        for (x=1;x!==wid;x++) {
+            y=halfHigh+Math.trunc(data[idx]*halfHigh);
+            ctx.lineTo(x,y);
+            
+            idx+=dataSkip;
         }
-        
-        setTimeout(this.addBitmapMetal.bind(this,idx),1);
-    }
-   
-    addBitmapMachine(idx)
-    {
-        this.drawSingleBitmap('Machine',this.genBitmapMachine.TYPE_NAMES[idx],this.genBitmapMachine.generate(idx,true));
-        
-        idx++;
-        if (idx>=this.genBitmapMachine.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapPanel.bind(this,0),1);
-            return;
-        }
-        
-        setTimeout(this.addBitmapMachine.bind(this,idx),1);
+
+        ctx.stroke();
+
+            // show the canvas
+            
+        this.bitmapCanvas.style.display='none';
+        this.soundCanvas.style.display='';
     }
     
-    addBitmapPanel(idx)
+    playSound()
     {
-        this.drawSingleBitmap('Panel',this.genBitmapPanel.TYPE_NAMES[idx],this.genBitmapPanel.generate(idx,true));
-        
-        idx++;
-        if (idx>=this.genBitmapPanel.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapBox.bind(this,0),1);
-            return;
-        }
-        
-        setTimeout(this.addBitmapPanel.bind(this,idx),1);
-    }
-    
-    addBitmapBox(idx)
-    {
-        this.drawSingleBitmap('Box',this.genBitmapBox.TYPE_NAMES[idx],this.genBitmapBox.generate(idx,true));
-        
-        idx++;
-        if (idx>=this.genBitmapBox.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapLiquids.bind(this,0),1);
-            return;
-        }
-        
-        setTimeout(this.addBitmapBox.bind(this,idx),1);
-    }
-    
-    addBitmapLiquids(idx)
-    {
-        this.drawSingleBitmap('Liquid',this.genBitmapLiquid.TYPE_NAMES[idx],this.genBitmapLiquid.generate(idx,true));
-        
-        idx++;
-        if (idx>=this.genBitmapLiquid.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapSkins.bind(this,0),1);
-            return;
-        }
-        
-        setTimeout(this.addBitmapLiquids.bind(this,idx),1);
-    }
-    
-    addBitmapSkins(idx)
-    {
-        this.drawSingleBitmap('Skin',this.genBitmapSkin.TYPE_NAMES[idx],this.genBitmapSkin.generate(idx,true));
-        
-        idx++;
-        if (idx>=this.genBitmapSkin.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapItems.bind(this,0),1);
-            return;
-        }
-        
-        setTimeout(this.addBitmapSkins.bind(this,idx),1);
-    }
-    
-    addBitmapItems(idx)
-    {
-        this.drawSingleBitmap('Item',this.genBitmapItem.TYPE_NAMES[idx],this.genBitmapItem.generate(idx,true));
-        
-        idx++;
-        if (idx>=this.genBitmapItem.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapSkies.bind(this,0),1);
-            return;
-        }
-        
-        setTimeout(this.addBitmapSkin.bind(this,idx),1);
-    }
-   
-    addBitmapSkies(idx)
-    {
-        this.drawSingleBitmap('Sky',this.genBitmapSky.TYPE_NAMES[idx],this.genBitmapSky.generate(idx,true));
-        
-        idx++;
-        if (idx>=this.genBitmapSky.TYPE_NAMES.length) {
-            setTimeout(this.addBitmapParticles.bind(this,0),1);
-            return;
-        }
-        
-        setTimeout(this.addBitmapSkies.bind(this,idx),1);
-    }
-    
-    addBitmapParticles(idx)
-    {
-        this.drawSingleBitmap('Particle',this.genBitmapParticle.TYPE_NAMES[idx],this.genBitmapParticle.generate(idx,true));
-        
-        idx++;
-        if (idx>=this.genBitmapParticle.TYPE_NAMES.length) return;
-        
-        setTimeout(this.addBitmapParticles.bind(this,idx),1);
+        sound.play(null,this.currentSoundBuffer);
     }
     
         //
@@ -276,7 +172,31 @@ class DebugRunClass
         
     clickList(idx)
     {
-        console.log(idx);
+        let item,itemDiv;
+        
+            // the list selection
+            
+        if (this.lastItemDiv!==null) this.lastItemDiv.style.backgroundColor=null;
+        
+        itemDiv=document.getElementById('item'+idx);
+        itemDiv.style.backgroundColor='#FF00FF';
+        
+        this.lastItemDiv=itemDiv;
+        
+            // draw the item
+            
+        item=this.list[idx];
+        
+        switch (item.objType) {
+            case this.DEBUG_ITEM_TYPE_BITMAP:
+                this.drawBitmap(item);
+                break;
+            case this.DEBUG_ITEM_TYPE_SOUND:
+                this.drawSound(item);
+                break;
+            case this.DEBUG_ITEM_TYPE_MODEL:
+                break;
+        }
     }
     
         //
@@ -300,8 +220,6 @@ class DebugRunClass
         this.listDiv.style.fontFamily='Arial';
         this.listDiv.style.fontSize='14pt';
         this.listDiv.style.whiteSpace='nowrap';
-        this.listDiv.style.paddingLeft='4px';
-        this.listDiv.style.paddingTop='4px';
         this.listDiv.style.boxSizing='border-box';
         this.listDiv.style.border='1px solid #AAAAAA';
         this.listDiv.style.overflowX='hidden';
@@ -314,17 +232,19 @@ class DebugRunClass
             
             switch (item.objType) {
                 case this.DEBUG_ITEM_TYPE_BITMAP:
-                    name='Bitmap:'+item.name;
+                    name='Bitmap>'+item.name+'>'+item.obj.TYPE_NAMES[item.typeIdx];
                     break;
                 case this.DEBUG_ITEM_TYPE_SOUND:
-                    name='Sound:'+item.name;
+                    name='Sound>'+item.obj.TYPE_NAMES[item.typeIdx];
                     break;
                 case this.DEBUG_ITEM_TYPE_MODEL:
-                    name='Model:'+item.name;
+                    name='Model>'+item.name;
                     break;
             }
             
             itemDiv=document.createElement('div');
+            itemDiv.id='item'+n;
+            itemDiv.style.paddingLeft='4px';
             itemDiv.style.cursor='pointer';
             itemDiv.appendChild(document.createTextNode(name));
             itemDiv.onclick=this.clickList.bind(this,n);
@@ -334,20 +254,32 @@ class DebugRunClass
         
         document.body.appendChild(this.listDiv);
         
-            // the drawing canvas
+            // the bitmap canvas
             
-        this.canvas=document.createElement('canvas');
-        this.canvas.style.position="absolute";
-        this.canvas.style.left='305px';
-        this.canvas.style.top='0px';
-        this.canvas.style.border='1px solid #000000';
-        this.canvas.width=800;
-        this.canvas.height=800;
+        this.bitmapCanvas=document.createElement('canvas');
+        this.bitmapCanvas.style.position="absolute";
+        this.bitmapCanvas.style.left='305px';
+        this.bitmapCanvas.style.top='0px';
+        this.bitmapCanvas.style.border='1px solid #000000';
+        this.bitmapCanvas.style.display='none';
+        this.bitmapCanvas.width=512;
+        this.bitmapCanvas.height=512;
         
-        document.body.appendChild(this.canvas);
-
-        //ctx=cvs.getContext('2d');
-        //ctx.drawImage(fromCanvas,0,0,wid,high);
+        document.body.appendChild(this.bitmapCanvas);
+        
+            // the sound canvas
+            
+        this.soundCanvas=document.createElement('canvas');
+        this.soundCanvas.style.position="absolute";
+        this.soundCanvas.style.left='305px';
+        this.soundCanvas.style.top='0px';
+        this.soundCanvas.style.border='1px solid #000000';
+        this.soundCanvas.style.display='none';
+        this.soundCanvas.width=512;
+        this.soundCanvas.height=256;
+        this.soundCanvas.onclick=this.playSound.bind(this);
+        
+        document.body.appendChild(this.soundCanvas);
     }
     
         //
@@ -357,8 +289,6 @@ class DebugRunClass
     run()
     {
         this.createInterface();
-        //this.addBitmapWall(0);
-        //this.addBitmapSkies(0);     // supergumba -- testing
     }
 }
 
